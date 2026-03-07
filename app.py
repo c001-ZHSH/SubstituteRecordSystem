e976af14da1a02813f1e657b8e690eb0cfaabc97
linwherehow <linwherehow@Mac.home>
2026-03-08 00:43:43 +0800

    Auto-launch browser on Windows Executable boot

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from app import create_app
+import webbrowser
+import threading
+import sys
 
 app = create_app()
 
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Add a short delay to let the Flask server boot up
+    threading.Timer(1.25, open_browser).start()
+    
+    # Disable debug mode when frozen (bundled) to avoid thread duplication
+    is_debug = not getattr(sys, 'frozen', False)
+    app.run(port=5000, debug=is_debug)
